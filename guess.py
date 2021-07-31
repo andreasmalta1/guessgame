3922c0a94d0b53e24f7d4a22328dde648317a8c1
andreasmalta1 <38852990+andreasmalta1@users.noreply.github.com>
2021-07-31 14:25:32 +0200

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,13 +1,16 @@
-# This is one of the simple python projects yet an exciting one. You can even call it a mini-game. Make a program in
-# which the computer randomly chooses a number between 1 to 10, 1 to 100, or any range. Then give users a hint to guess
-# the number. Every time the user guesses wrong, he gets another clue, and his score gets reduced. The clue can be
-# multiples, divisible, greater or smaller, or a combination of all.
+# A high or low guessing game
+# There are two versions of the game - user_guess or computer_guess.
+# In user_guess the user has to to guess the random generated number which cab be between any chosen range
+# With each guess, the computer responds whether the guess is too low, too high or correct
+# In computer_guess the user thinks of a number and the the computer will try to guess using random numbers
+# The user responds with high, low or correct
 
 import random
 
 
+# The user tries to guess
 def user_guess(x):
-    random_number = random.randint(1, x)
+    random_number = random.randint(1, x)  # Generating random number from 1 to x
     guess = 0
     while guess != random_number:
         guess = int(input(f"Guess a number between 1 and {x}: "))
@@ -19,7 +22,10 @@ def user_guess(x):
     print(f"You guessed the number {random_number} correctly")
 
 
+# Computer tries to guess
 def computer_guess(x):
+    # Initially the low number is 1 and the high is the highest number
+    # With each guess and user response, the range of options for the computer to guess will get stricter
     low = 1
     high = x
     feedback = ""
@@ -28,7 +34,7 @@ def computer_guess(x):
             guess = random.randint(low, high)
         else:
             guess = low
-        feedback = input(f" Is {guess} too high (H), too low (L) or correct (C)?: ").lower()
+        feedback = input(f"Is {guess} too high (H), too low (L) or correct (C)?: ").lower()
         if feedback == "h":
             high = guess - 1
         if feedback == "l":
@@ -36,5 +42,6 @@ def computer_guess(x):
     print(f"Computer guessed {guess}")
 
 
-computer_guess(1000)
-# user_guess(10)
+# computer_guess(1000)
+user_guess(100)
+
